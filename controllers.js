2cc0585e5210f12108e26342674a5e4b89c16e7e
Pearce Keesling <keeslinp@gmail.com>
2016-05-26 15:16:33 -0600

    Introduced CSS to animate when a task is completed

--- controllers.js
+++ controllers.js
@@ -1,13 +1,15 @@
 var ToDoApp = angular.module('ToDoApp', []);
 
 ToDoApp.controller('TaskListController', function TaskListController($scope) {
-  $scope.tasks = ['test','test2','test3'];
+  $scope.tasks = [{text:'test',done:''}];
   $scope.addTask = function () {
-    $scope.tasks.push($scope.newTask);
+    $scope.tasks.push({text:$scope.newTask,done:''});
     $scope.newTask = '';
   };
-  $scope.removeTask = function(index){
-    $scope.tasks.splice(index,1);
-    $scope.checked=false;
-  };
+  $scope.completeTask = function(index){
+    //TODO figure out how to remove item at end of animation
+    //$scope.tasks.splice(index,1);
+    //$scope.checked=false;
+    $scope.tasks[index].done=($scope.tasks[index].done=='' ? 'done' : '');
+    };
 });
