07b182494f1b1102fae575f0a767d7d93b609073
Pearce Keesling <pearce@PearceLinux.home>
2016-05-26 00:48:03 -0600

    Wrote the basic add and remove features for the list.

---
+++ controllers.js
@@ -0,0 +1,12 @@
+var ToDoApp = angular.module('ToDoApp', []);
+
+ToDoApp.controller('TaskListController', function TaskListController($scope) {
+  $scope.tasks = ['test','test2','test3'];
+  $scope.addTask = function () {
+    $scope.tasks.push($scope.newTask);
+    $scope.newTask = '';
+  };
+  $scope.removeTask = function(index){
+    $scope.tasks.splice(index,1);
+  };
+});
