45013e2e3160bd911fcc2ed0606a6a075aafef9c
Pearce Keesling <keeslinp@gmail.com>
2016-05-26 17:23:51 -0600

    Now I actually have the animations doing everything I want :). Fixed it all up to use proper Angular animations.

--- controllers.js
+++ controllers.js
@@ -1,15 +1,25 @@
-var ToDoApp = angular.module('ToDoApp', []);
+var ToDoApp = angular.module('ToDoApp', ['ngAnimate']);
 
 ToDoApp.controller('TaskListController', function TaskListController($scope) {
-  $scope.tasks = [{text:'test',done:''}];
+  $scope.tasks = [{text:"Example Task. Click me to complete me :).",u_id:guid()}];
   $scope.addTask = function () {
-    $scope.tasks.push({text:$scope.newTask,done:''});
+    $scope.tasks.push({text:$scope.newTask,u_id:guid()});
     $scope.newTask = '';
   };
   $scope.completeTask = function(index){
     //TODO figure out how to remove item at end of animation
-    //$scope.tasks.splice(index,1);
+    $scope.tasks.splice(index,1);
     //$scope.checked=false;
-    $scope.tasks[index].done=($scope.tasks[index].done=='' ? 'done' : '');
+    //$scope.tasks[index].done=($scope.tasks[index].done=='' ? 'done' : '');
     };
 });
+
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
+}
