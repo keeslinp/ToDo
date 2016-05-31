22b3007b51fba7f3bbc6e0d110b5855e00953bc2
Pearce Keesling <keeslinp@gmail.com>
2016-05-31 13:40:48 -0600

    Added in a counter directive to keep track of how old a task is. Not useful but functional and was good practice.

--- app/components/tasks/tasksDirectives.js
+++ app/components/tasks/tasksDirectives.js
@@ -3,4 +3,29 @@ angular.module("ToDoApp").directive('tdaTasks', function(){
   return{
         templateUrl: 'app/components/tasks/taskListTemplate.html'
   };
-});
+}).
+
+directive('tdaTimer', ['$interval', function($interval) {
+
+  function link(scope, element, attrs) {
+    var timeoutId;
+    var time = 0;
+    function updateTime() {
+      element.text(time++ + " second"+(time==2?"":"s") + " since you started the task.");
+    }
+
+    element.on('$destroy', function() {
+      $interval.cancel(timeoutId);
+    });
+
+    // start the UI update process; save the timeoutId for canceling
+    timeoutId = $interval(function() {
+      updateTime(); // update DOM
+    }, 1000);
+    updateTime();
+  }
+
+  return {
+    link: link
+  };
+}]);
